3ea1335983512244fcbd589035619f88a672fd23
Bobby Wilson <bobby@entryway.net>
2010-09-23 15:16:28 -0600

    updated require again

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,3 @@
 require File.expand_path(File.dirname(__FILE__) + "/../pebbles_audio_files")
-require File.expand_path(File.dirname(__FILE__) + "/../generators")
+require File.expand_path(File.dirname(__FILE__) + "/../generators/pebbles_audio_files_generator.rb")
 Product.send(:include, PebblesAudioFiles::Models::Product)
