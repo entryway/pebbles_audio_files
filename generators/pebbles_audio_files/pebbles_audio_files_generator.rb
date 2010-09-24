eee3f925704b45e0a189d3f20ae77e27a8ba429a
Bobby Wilson <bobby@entryway.net>
2010-09-24 08:31:10 -0600

    changed generator back to full definition

--- generators/pebbles_audio_files/pebbles_audio_files_generator.rb
+++ generators/pebbles_audio_files/pebbles_audio_files_generator.rb
@@ -1,34 +1,24 @@
-#require File.expand_path(File.dirname(__FILE__) + "/lib/insert_commands.rb")
-#require File.expand_path(File.dirname(__FILE__) + "/lib/rake_commands.rb")
-#require 'rails_generator'
-#require 'rails_generator/scripts/generate'
+require File.expand_path(File.dirname(__FILE__) + "/lib/insert_commands.rb")
+require File.expand_path(File.dirname(__FILE__) + "/lib/rake_commands.rb")
+require 'rails_generator'
+require 'rails_generator/scripts/generate'
 
-#class PebblesAudioFilesGenerator < Rails::Generator::Base
+class PebblesAudioFilesGenerator < Rails::Generator::Base
 
-  #def manifest
-    #record do |m|
-      #audio_clip_model = "app/models/audio_clip.rb"
-      #if File.exists?(audio_clip_model)
-        #m.insert_into audio_clip_model, "include PebblesAudioClip::Models::AudioClip"
-      #else
-        #m.directory File.join("app","models")
-        #m.file "audio_clip.rb", audio_clip_model
-      #end
-
-      #m.migration_template "migrations/create_audio_clips.rb",
-        #'db/migrate',
-        #:migration_file_name => "pebbles_audio_files_create_audio_clips"
-
-    #end
-  #end
-#end
-
-class PebblesAudioFilesGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      # m.directory "lib"
-      # m.template 'README', "README"
+      audio_clip_model = "app/models/audio_clip.rb"
+      if File.exists?(audio_clip_model)
+        m.insert_into audio_clip_model, "include PebblesAudioClip::Models::AudioClip"
+      else
+        m.directory File.join("app","models")
+        m.file "audio_clip.rb", audio_clip_model
+      end
+
+      m.migration_template "migrations/create_audio_clips.rb",
+        'db/migrate',
+        :migration_file_name => "pebbles_audio_files_create_audio_clips"
+
     end
   end
 end
-
